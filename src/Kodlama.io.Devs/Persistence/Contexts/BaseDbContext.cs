869d1799da3f313a559132ee7663449219e5d109
Muhammet Arık <76816388+MuhammetArik@users.noreply.github.com>
2022-09-11 10:13:00 +0300

    Add Model Entity

--- src/Kodlama.io.Devs/Persistence/Contexts/BaseDbContext.cs
+++ src/Kodlama.io.Devs/Persistence/Contexts/BaseDbContext.cs
@@ -14,6 +14,7 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+        public DbSet<Technology> Technologies { get; set; }
 
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
@@ -35,6 +36,16 @@ namespace Persistence.Contexts
                 a.ToTable("ProgrammingLanguages").HasKey(k => k.Id);
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
+                a.HasMany(p => p.Technologies);
+            });
+
+            modelBuilder.Entity<Technology>(a =>
+            {
+                a.ToTable("Technologies").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.ProgrammingLanguageId).HasColumnName("ProgrammingLanguageId");
+                a.Property(p => p.Name).HasColumnName("Name");
+                a.HasOne(p => p.ProgrammingLanguage);
             });
 
 
@@ -42,6 +53,8 @@ namespace Persistence.Contexts
             ProgrammingLanguage[] programmingLanguageEntitySeeds = { new(1, "C#"), new(2, "Java") };
             modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageEntitySeeds);
 
+            Technology[] technologyEntitySeeds = { new(1,1,"ASP.NET"), new(2, 2, "Spring"), };
+            modelBuilder.Entity<Technology>().HasData(technologyEntitySeeds);
 
         }
     }
